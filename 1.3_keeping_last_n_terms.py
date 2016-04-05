7f2481b7b60116882eb6a305fdcff4d1c9762c9a
Varun Singhal <varunsinghal15@gmail.com>
2016-04-05 13:41:44 +0530

    Update 1.3_keeping_last_n_terms.py
    Added comments for output statements

--- 1.3_keeping_last_n_terms.py
+++ 1.3_keeping_last_n_terms.py
@@ -2,12 +2,18 @@
 
 from collections import deque
 d = deque(maxlen=3)
+
 d.append(1)
 d.append(2)
 d.append(3)
+
 print d
+#Output: deque([1, 2, 3], maxlen=3)
+
 d.append(4)
+
 print d
+#Output: deque([2, 3, 4], maxlen=3)
 
 '''
 The complexity of inserting or removing an element from the list data type is O(N). 
